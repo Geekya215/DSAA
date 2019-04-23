3f12d6e0d5b56fd1dc990affe0cfedc5b7222a05
Lin Yang <1074504492@qq.com>
2019-04-23 22:50:50 +0800

    update graph

---
+++ Graph/dijkstra.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int size = 1e2+5;
+
+typedef pair<int,int> pos;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
