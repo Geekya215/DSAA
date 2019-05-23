019717e7089e0627e97a50089de56f05ecc480a4
Lin Yang <1074504492@qq.com>
2019-05-23 11:44:17 +0800

    fix 8dp bug

--- Algorithm/Search/BFS/8-digtal.cpp
+++ Algorithm/Search/BFS/8-digtal.cpp
@@ -1,27 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define pb push_back
+
 typedef vector<vector<int>> state;
+
+map<int, int> dis;
 
 state start;
 state goal;
 
-int dir[][2] = {1,0,0,1,-1,0,0,-1};
 
-map<int, int> dis;
+int dir[][2] = {0,1,1,0,-1,0,0,-1};
 
 int hash_f(state& s) {
-	long long num = 0;
+	int num = 0;
 	for(int i = 0; i < 3; ++i) {
-		for(int j = 0; j < 3; ++j)
-			num = num*10 + s[i][j];
+		for(int j = 0; j < 3; ++j) {
+			num = num *10 + s[i][j];
+		}
 	}
 	return num;
 }
 
-void getzero(state& s, int &x, int &y) {
 
-	for(int i = 0 ; i < 3; ++i) {
+void read(state& s) {
+	int n;
+	for(int i = 0; i < 3; ++i) {
+		vector<int> temp;
+		for(int j = 0; j < 3; ++j) {
+			cin >> n;
+			temp.pb(n);
+		}
+		s.pb(temp);
+	}
+}
+
+
+void getzero(state& s, int& x, int& y) {
+	for(int i = 0; i < 3; ++i) {
 		for(int j = 0; j < 3; ++j) {
 			if(s[i][j] == 0) {
 				x = i;
@@ -32,8 +49,9 @@ void getzero(state& s, int &x, int &y) {
 	}
 }
 
-int check(state& s, int x, int y) {
-	if(x<0 || x>=3 || y<0 || y>=3 || dis.find(hash_f(s)) != dis.end())
+
+int check(int x, int y) {
+	if(x<0 || x>=3 || y<0 || y>=3)
 		return 0;
 	return 1;
 }
@@ -42,55 +60,43 @@ int check(state& s, int x, int y) {
 void bfs() {
 	queue<state> q;
 	q.push(start);
-	int tx, ty;
-	while(1) {
-		state s = q.front();
-		q.pop();
 
-		if(dis.find(hash_f(goal)) != dis.end()) {
+	int zx,zy;
+
+	while(1) {
+		if(dis[hash_f(goal)] != 0) {
 			break;
 		}
 
-		getzero(s,tx,ty);
+		state s = q.front();
+		q.pop();
+
+		getzero(s, zx, zy);
 
 		for(int i = 0; i < 4; ++i) {
-			int nx = tx + dir[i][0];
-			int ny = ty + dir[i][1];
-			state temp = s;
-			temp[tx][ty] ^= s[nx][ny] ^= temp[tx][ty] ^= s[nx][ny];
-			if(check(temp,nx,ny)) {
-				// swap position
-				q.push(temp);
-				dis[hash_f(temp)] = dis[hash_f(s)] + 1;
+			int nx = zx + dir[i][0];
+			int ny = zy + dir[i][1];
+
+			if(check(nx, ny)) {
+				state temp = s;
+				temp[nx][ny] ^= temp[zx][zy] ^= temp[nx][ny] ^= temp[zx][zy];
+				if(dis[hash_f(temp)] == 0) {
+					dis[hash_f(temp)] = dis[hash_f(s)] + 1;
+					q.push(temp);
+				}
 			}
 		}
 	}
 }
 
+
 int main() {
+	// fropen("in.txt","r",stdin);
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	int num;
-	for(int i = 0; i < 3; ++i) {
-		vector<int> temp;
-		for(int j = 0; j < 3; ++j) {
-			cin >> num;
-			temp.push_back(num);
-		}
-		start.push_back(temp);
-	}
-
-	for(int i = 0; i < 3; ++i) {
-		vector<int> temp;
-		for(int j = 0; j < 3; ++j) {
-			cin >> num;
-			temp.push_back(num);
-		}
-		goal.push_back(temp);
-	}
-
+	read(start);
+	read(goal);
 	bfs();
-
 	cout << dis[hash_f(goal)] << endl;
-	return 0;
+ 	return 0;
 }
